ffea504a0a13f1b327525cbfdd05bed9105581f4
Marcelo Brito <marcellus.brito@gmail.com>
2023-12-31 22:45:51 -0300

    feat: assets & navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons';
+import './navbar.css';
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fd__navbar'>
+      <div className='fd__navbar-links'>
+        <div className='fd__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='fd__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#whatFutureDrive">What is FD?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='fd__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+    </div>
   )
 }
 
