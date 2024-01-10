bc8189924189716e0bdc186cfe1d7136d6a7fe96
Marcelo Brito <marcellus.brito@gmail.com>
2024-01-09 23:11:30 -0300

    feat: cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='fd__cta'>
+      <div className='fd__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='fd__cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
