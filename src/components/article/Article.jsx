d9f979c26376f1407c37aa493dfe6cd862991980
Marcelo Brito <marcellus.brito@gmail.com>
2024-01-10 22:43:00 -0300

    feat: blog/article section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgURL, date, title }) => {
   return (
-    <div>Article</div>
+    <div className='fd__blog-container_article'>
+      <div className='fd__blog-container_article-image'>
+        <img src={imgURL} alt='blog'  />
+      </div>
+      <div className='fd__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './blog.css'
+import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
+import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='fd__blog section__padding' id="blog">
+      <div className='fd__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, <br />We are blogging about it.</h1>
+      </div>
+      <div className='fd__blog-container'>
+        <div className='fd__blog-container_groupA'>
+          <Article imgURL={blog01} date='Dez 09, 2023' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+        </div>
+        <div className='fd__blog-container_groupB'>
+          <Article imgURL={blog02} date='Dez 09, 2023' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+          <Article imgURL={blog03} date='Dez 09, 2023' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+          <Article imgURL={blog04} date='Dez 09, 2023' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+          <Article imgURL={blog05} date='Dez 09, 2023' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+        </div>
+      </div>
+    </div>
   )
 }
 
